10b66b474c412c814df7d6b06a9c30dadaaa8a81
Devansh <devansh@trainman.in>
2019-10-19 22:53:26 +0530

    added constrain set for home state

--- app/src/main/java/com/example/snapmotion/MainActivity.kt
+++ app/src/main/java/com/example/snapmotion/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.snapmotion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //val binding  = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        favouritesActionContainer.setOnClickListener {
+
+
+        }
+
+        homeActionContainer.setOnClickListener {
+
+
+        }
+
+        bookmarkActionContainer.setOnClickListener {
+
+
+        }
+
     }
 }
